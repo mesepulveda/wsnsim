aa08166b931bd5b4f53dfad6d8c45766b3975e95
Matías Sepúlveda <mesepulveda@uc.cl>
2020-08-26 19:53:47 -0400

    first draft of a simple test

---
+++ tests/simple_test.py
@@ -0,0 +1,21 @@
+"""Some very simple test of the simulator."""
+
+from simulator.node.node import SinkNode, SensingNode
+from simulator.link.link import Link
+from simulator.network.network import Network
+
+
+def test_1():
+    # Create two nodes
+    sink = SinkNode(0, 'sink')
+    sensing = SensingNode(1, 'node 1')
+    # Create one link
+    link = Link(sink, sensing)
+    # Create network
+    network = Network({sink, sensing}, {link})
+    # Show information about the network
+    network.display_summary()
+
+
+if __name__ == '__main__':
+    test_1()
