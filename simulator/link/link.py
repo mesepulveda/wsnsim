a3d1431a81826979a1b8bc0200cdaeec71cf7e34
Matías Sepúlveda <mesepulveda@uc.cl>
2020-08-26 20:51:39 -0400

    optional attributes added with kwargs

--- simulator/link/link.py
+++ simulator/link/link.py
@@ -3,8 +3,10 @@
 
 class Link:
     """Relates two nodes in a physical medium."""
-    def __init__(self, node_1, node_2):
+    def __init__(self, node_1, node_2, **kwargs):
         self.nodes = sorted([node_1, node_2], key=lambda node: node.address)
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     def __repr__(self):
         return '{0}, {1}'.format(*[node.address for node in self.nodes])
