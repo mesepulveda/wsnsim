9b0cbe425568fa3b5d33bae8984aeb5d907d560c
Matías Sepúlveda <mesepulveda@uc.cl>
2020-08-26 21:41:12 -0400

    decorator moved to auxiliary_functions.py and get_delay as a method of a link

--- simulator/auxiliary_functions.py
+++ simulator/auxiliary_functions.py
@@ -2,7 +2,10 @@
 
 
 def add_asterisks(function):
-    """Used as a decorator."""
+    """Used as a decorator.
+
+    Adds asterisks before and after the execution of a function.
+    """
     def inner(*args, **kwargs):
         """Adds asterisks before and after the execution of a function."""
         number_of_asterisks = 50
@@ -10,4 +13,18 @@ def add_asterisks(function):
         function(*args, **kwargs)
         print('*' * number_of_asterisks)
 
+    return inner
+
+
+def ensure_positive_value(function):
+    """Used as a decorator.
+
+    Ensures that the value is 0 or positive.
+    """
+    def inner(*args, **kwargs):
+        """Ensures that the value is 0 or positive."""
+        value = function(*args, **kwargs)
+        if value < 0:
+            raise ValueError('Delay value obtained is negative')
+        return value
     return inner
--- simulator/link/link.py
+++ simulator/link/link.py
@@ -1,13 +1,19 @@
 """Everything related with the simulation of a link."""
+from simulator.auxiliary_functions import ensure_positive_value
 
 
 class Link:
     """Relates two nodes in a physical medium."""
     def __init__(self, node_1, node_2, delay_function, **kwargs):
         self.nodes = sorted([node_1, node_2], key=lambda node: node.address)
-        self.delay_function = ensure_positive_value(delay_function)
+        self._delay_function = delay_function
         for key, value in kwargs.items():
             setattr(self, key, value)
+
+    @ensure_positive_value
+    def get_delay(self):
+        """Returns a realization of delay value of the link."""
+        return self.delay_function()
 
     def __repr__(self):
         return '{0}, {1}'.format(*[node.address for node in self.nodes])
@@ -16,12 +22,3 @@ class Link:
         return '{0}, {1}'.format(*[node.address for node in self.nodes])
 
 
-def ensure_positive_value(function):
-    """Decorator."""
-    def inner(*args, **kwargs):
-        """Ensures that the value is 0 or positive."""
-        value = function(*args, **kwargs)
-        if value < 0:
-            raise ValueError('Delay value obtained is negative')
-        return value
-    return inner
