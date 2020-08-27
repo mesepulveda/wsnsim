a1c737e0e62481027e5eb4bf2fa31b20fa9e23bd
Matías Sepúlveda <mesepulveda@uc.cl>
2020-08-26 23:27:34 -0400

    Simulation classes added and Node changed to private class

--- simulator/node/node.py
+++ simulator/node/node.py
@@ -1,7 +1,7 @@
 """Everything related with the simulation of a node."""
 
 
-class Node:
+class _Node:
     """Defines attributes and methods needed in both sink and sensing nodes."""
 
     def __init__(self, address, name=None):
@@ -23,7 +23,7 @@ class Node:
         return '{0}, {1}'.format(self.address, self.name)
 
 
-class SensingNode(Node):
+class SensingNode(_Node):
     """Defines attributes and methods of a regular sensing (not sink) node."""
 
     def __init__(self, address, name=None):
@@ -34,9 +34,46 @@ class SensingNode(Node):
         pass
 
 
-class SinkNode(Node):
+class SinkNode(_Node):
     """Defines attributes and methods specific for a sink node."""
 
     def __init__(self, address, name=None):
         super().__init__(address, name)
         pass
+
+
+class _SimulationNode(_Node):
+    """Extends Node class in order to simulate."""
+
+    def __init__(self, address, name=None):
+        super().__init__(address, name)
+
+
+class SimulationSensingNode(SensingNode, _SimulationNode):
+    """Extends SensingNode and SimulationNode class in order to simulate."""
+
+    def __init__(self, address, name=None):
+        super().__init__(address, name)
+
+
+class SimulationSinkNode(SinkNode, _SimulationNode):
+    """Extends SinkNode and SimulationNode class in order to simulate."""
+
+    def __init__(self, address, name=None):
+        super().__init__(address, name)
+
+
+def convert_to_simulation_nodes(nodes):
+    """Returns simulation nodes from regular nodes."""
+    simulation_nodes = []
+    for node in nodes:
+        address = node.address
+        name = node.name
+        if isinstance(node, SensingNode):
+            simulation_node = SimulationSensingNode(address, name)
+        elif isinstance(node, SinkNode):
+            simulation_node = SimulationSinkNode(address, name)
+        else:
+            raise AttributeError('Class of node is not correct')
+        simulation_nodes.append(simulation_node)
+    return simulation_nodes
