3cc75719e25c94b643ad64370ff15faf1367b7d2
Matías Sepúlveda <mesepulveda@uc.cl>
2020-09-03 18:21:07 -0400

    base event now generates an ID using uuid4

--- simulator/event/base_event.py
+++ simulator/event/base_event.py
@@ -1,10 +1,11 @@
 """Module with code related to the events of the simulator."""
+from uuid import uuid4
 
 
 class _BaseEvent:
     """Base class for the events of the simulator."""
 
-    def __init__(self, event_id, event_time):
-        self.id = event_id
+    def __init__(self, event_time):
+        self.id = uuid4()
         self.time = event_time
         pass
