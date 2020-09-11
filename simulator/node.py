6c5fcc43dfe759591c57f881d2d38323be7f7a41
Matías Sepúlveda <mesepulveda@uc.cl>
2020-09-10 23:52:57 -0300

    readability improved

--- simulator/node.py
+++ simulator/node.py
@@ -19,17 +19,17 @@ class _Node:
         else:
             self.name = self.address
 
-    def __repr__(self):
-        return '{0}, {1}'.format(self.address, self.name)
-
-    def __str__(self):
-        return '{0}, {1}'.format(self.address, self.name)
-
     def __eq__(self, other):
         return self.address == other.address
 
     def __hash__(self):
         return hash(self.address)
+
+    def __repr__(self):
+        return f'(Address: {self.address}, Name: {self.name})'
+
+    def __str__(self):
+        return f'Node {self.name}'
 
 
 class SensingNode(_Node):
@@ -44,9 +44,6 @@ class SinkNode(_Node):
 
     def __init__(self, address: str, name: Optional[str] = None) -> None:
         super().__init__(address, name)
-
-
-Node = Union[SensingNode, SinkNode]
 
 
 class _SimulationNode(_Node):
@@ -64,14 +61,17 @@ class _SimulationNode(_Node):
             -> Generator[Event, Any, Any]:
         """Sends a message to sink or neighbour nodes."""
         # Pass the message to the routing protocol
-        yield self.env.process(self.routing_protocol.add_to_output_queue(
-            message,
-            destination))
+        event = self.routing_protocol.add_to_output_queue(message, destination)
+        yield self.env.process(event)
 
     def receive_message(self, message: str) -> None:
         """Receive a message from another node."""
         # Pass the message to the routing protocol in order to analyze it
         self.routing_protocol.receive_packet(message)
+
+    def _print_info(self, info: str) -> None:
+        """Print information with format."""
+        print(f'{self.env.now:.2f} | {self} | {info}')
 
 
 class SimulationSensingNode(_SimulationNode, SensingNode):
@@ -88,16 +88,17 @@ class SimulationSensingNode(_SimulationNode, SensingNode):
         """Main routine of the nodes."""
         # random delay between 0 and 10
         yield self.env.timeout(random() * 10)
-        print(round(self.env.now, 2), self.name, 'is awake')
+        self._print_info('is awake')
         yield self.env.timeout(60)  # Wait one minute before begin sensing
         while True:
             # Sensing every 15 minutes
-            self.env.process(
-                self._send_message(f'{self.address}/'       # Node id
-                                   f'X/'                    # Measurement
-                                   f'{self.env.now:.2f}',  # Timestamp
-                                   'sink'))
+            event = self._send_message(self._format_measurement('X'), 'sink')
+            self.env.process(event)
             yield self.env.timeout(15 * 60)
+
+    def _format_measurement(self, measurement: str) -> str:
+        """Formats the measurement to include address and timestamp."""
+        return f'{self.address}/{measurement}/{self.env.now:.2f}'
 
 
 class SimulationSinkNode(_SimulationNode, SinkNode):
@@ -115,12 +116,14 @@ class SimulationSinkNode(_SimulationNode, SinkNode):
         """Main routine of the nodes."""
         # random delay between 0 and 10
         yield self.env.timeout(random() * 10)
-        print(round(self.env.now, 2), self.name, 'is awake')
-        # wait for 10s in order to wait for the other nodes to wake up
+        self._print_info('is awake')
+        # Wait for 10s in order to wait for the other nodes to wake up
         yield self.env.timeout(10)
+        # Start neighbour discovery and hop count update
         self.env.process(self.routing_protocol.setup())
 
 
+Node = Union[SensingNode, SinkNode]
 SimulationNode = Union[SimulationSinkNode, SimulationSensingNode]
 
 
@@ -128,7 +131,8 @@ def convert_to_simulation_nodes(
         regular_nodes: Iterable[Node],
         routing_protocol: str,
         send_data_function: Callable[[str], Generator[Event, Any, Any]],
-        env: Environment) -> Iterable[SimulationNode]:
+        env: Environment) \
+        -> Iterable[SimulationNode]:
     """Returns simulation nodes from regular nodes."""
     simulation_nodes = []
     if routing_protocol == 'min-hop':
@@ -138,32 +142,28 @@ def convert_to_simulation_nodes(
         routing_sensing_node = MinHopRouting
         routing_sink_node = MinHopRoutingSink
     for node in regular_nodes:
-        address = node.address
-        name = node.name
         if isinstance(node, SensingNode):
-            simulation_node = SimulationSensingNode(address,
-                                                    name,
-                                                    routing_sensing_node,
-                                                    send_data_function,
-                                                    env)
+            TypeOfNode = SimulationSensingNode
+            routing = routing_sensing_node
         elif isinstance(node, SinkNode):
-            simulation_node = SimulationSinkNode(address,
-                                                 name,
-                                                 routing_sink_node,
-                                                 send_data_function,
-                                                 env)
+            TypeOfNode = SimulationSinkNode
+            routing = routing_sink_node
         else:
             raise AttributeError('Class of node is not correct')
+        simulation_node = TypeOfNode(node.address,
+                                     node.name,
+                                     routing,
+                                     send_data_function,
+                                     env)
         simulation_nodes.append(simulation_node)
     return simulation_nodes
 
 
-def get_equivalent_simulation_node(nodes, simulation_nodes):
-    """Returns the equivalent simulation node of one or many nodes."""
-    # If it is only one object
-    if isinstance(nodes, (SensingNode, SinkNode)):
-        return _get_equivalent_node(nodes, simulation_nodes)
-    # If it is a list of objects
+def get_equivalent_simulation_node(
+        nodes: Iterable[Node],
+        simulation_nodes: Iterable[SimulationNode]) \
+        -> Iterable[SimulationNode]:
+    """Returns the equivalent simulation node of a list of nodes."""
     equivalent_simulation_nodes = []
     for node in nodes:
         equivalent_node = _get_equivalent_node(node, simulation_nodes)
@@ -171,7 +171,10 @@ def get_equivalent_simulation_node(nodes, simulation_nodes):
     return equivalent_simulation_nodes
 
 
-def _get_equivalent_node(node, simulation_nodes):
+def _get_equivalent_node(
+        node: Node,
+        simulation_nodes: Iterable[SimulationNode]) \
+        -> SimulationNode:
     """Returns the equivalent simulation node of one node."""
     for simulation_node in simulation_nodes:
         if node.address == simulation_node.address:
