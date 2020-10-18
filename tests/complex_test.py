15e337a017046c6518e3771f1bcd885a771a7dbd
Matías Sepúlveda <mesepulveda@uc.cl>
2020-10-18 18:40:33 -0300

    topology of example changed in order to have different results with min-hop and ETX

--- tests/complex_test.py
+++ tests/complex_test.py
@@ -5,9 +5,11 @@ from wsnsim import SinkNode, SensingNode, Link, Network, Simulation
 
 def test_1():
     """
+          (2)
          1 - 3
-       /     |
+     (1)/    |(3)
      0 - 2 - 4 - 5
+      (4) (5) (6)
     """
     # Create nodes
     sink = SinkNode('0', name='sink')
@@ -18,10 +20,10 @@ def test_1():
     sensing_5 = SensingNode('5', sensing_period=60*60, sensing_offset=50*60)
     # Create links
     link_1 = Link(sink, sensing_1, lambda: 1)
-    link_2 = Link(sink, sensing_2, lambda: 2)
-    link_3 = Link(sensing_1, sensing_3, lambda: 3)
-    link_4 = Link(sensing_2, sensing_4, lambda: 4)
-    link_5 = Link(sensing_3, sensing_4, lambda: 5)
+    link_2 = Link(sensing_1, sensing_3, lambda: 2)
+    link_3 = Link(sensing_3, sensing_4, lambda: 3)
+    link_4 = Link(sink, sensing_2, lambda: 4)
+    link_5 = Link(sensing_2, sensing_4, lambda: 5)
     link_6 = Link(sensing_4, sensing_5, lambda: 6)
     # Create network
     network = Network({sink,
